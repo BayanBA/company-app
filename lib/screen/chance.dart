e4ce4f191aab272f5e3fb20fb69dcef65e130971
BayanBA <itebayan@gmail.com>
2021-06-14 16:29:45 +0300

    chanc....

--- lib/screen/chance.dart
+++ lib/screen/chance.dart
@@ -1,3 +1,4 @@
+
 import 'package:b/screen/view.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:im_stepper/stepper.dart';
@@ -6,6 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:jiffy/jiffy.dart';
 import 'package:provider/provider.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
 
 class AddJop extends StatefulWidget {
   @override
@@ -39,6 +41,7 @@ class _AddJopState extends State<AddJop> {
     "Vacancies": 1,
     "dateOfPublication": "",
   };
+
 
   var aa = new List();
   Stander stan = new Stander();
@@ -113,22 +116,30 @@ class _AddJopState extends State<AddJop> {
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("معلومات فرصة العمل"),
-      ),
-      body: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15.8),
-        ),
-        child: des(),
-      ),
-    );
-  }
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: AppBar(
+  //       title: Text("معلومات فرصة العمل"),
+  //     ),
+  //     body:
+  //       ListView(
+  //         children: [
+  //           Container(
+  //             height: 400,
+  //             decoration: BoxDecoration(
+  //                 borderRadius: BorderRadius.circular(15.8),
+  //                 color: Colors.black12
+  //             ),
+  //             child: des(),
+  //           ),
+  //           ppp
+  //         ],
+  //     ),
+  //   );
+  // }
 
-  Widget x(bbb, String name, String hint, Icon c) {
+  Widget x(bbb, String name, String hint, Icon c,var k,int i) {
     return TextFormField(
       initialValue: d[bbb],
       decoration: InputDecoration(
@@ -173,7 +184,18 @@ class _AddJopState extends State<AddJop> {
       onSaved: (valu) {
         d[bbb] = valu;
       },
+      onEditingComplete: (){
+        _index++;
+        drow(k,i);
+      },
     );
+  }
+
+
+  drow(var k,int i){
+    var kk=k.currentState;
+    if(kk.validate() )
+      edit(i);
   }
 
   Widget z(var v, String name, List<dynamic> l) {
@@ -194,10 +216,10 @@ class _AddJopState extends State<AddJop> {
     );
   }
 
-  Widget q(String name) {
+  Widget q(String name,var k,int i) {
     if (name == "خبير")
       return x(
-          "expir", "عدد سنوات الخبرة:", ".......", Icon(Icons.auto_awesome));
+          "expir", "عدد سنوات الخبرة:", ".......", Icon(Icons.auto_awesome),k,i);
     return Text("");
   }
 
@@ -235,6 +257,7 @@ class _AddJopState extends State<AddJop> {
       "level": stan.level,
       "Vacancies": stan.Vacancies,
       "dateOfPublication": Jiffy(date).fromNow(),
+      "list":""
     });
     Navigator.of(context).pushReplacement(
         MaterialPageRoute(builder: (context) => ShowingData()));
@@ -245,16 +268,22 @@ class _AddJopState extends State<AddJop> {
       Form(
         autovalidateMode: AutovalidateMode.always,
         key: k1,
-        child: SizedBox(
-            width: 300,
-            child: x("title", "العنوان:", ".......", Icon(Icons.title))),
+        child: Column(
+          children: [
+            Text("عنوان الفرصة:"),
+            SizedBox(height: 100,),
+            SizedBox(
+                width: 300,
+                child: x("title", "العنوان:", ".......", Icon(Icons.title),k1,_index)),
+          ],
+        ),
       ),
       Form(
         autovalidateMode: AutovalidateMode.always,
         key: k2,
         child: SizedBox(
             width: 300,
-            child: x("age", "العمر:", ".......", Icon(Icons.person))),
+            child: x("age", "العمر:", ".......", Icon(Icons.person),k2,_index)),
       ),
       Form(
         autovalidateMode: AutovalidateMode.always,
@@ -262,14 +291,14 @@ class _AddJopState extends State<AddJop> {
         child: SizedBox(
             width: 300,
             child: x("workTime", "عدد ساعات العمل:", ".......",
-                Icon(Icons.alarm_on_sharp))),
+                Icon(Icons.alarm_on_sharp),k3,_index)),
       ),
       Form(
         autovalidateMode: AutovalidateMode.always,
         key: k4,
         child: SizedBox(
             width: 300,
-            child: x("salary", "الراتب:", ".......", Icon(Icons.money))),
+            child: x("salary", "الراتب:", ".......", Icon(Icons.money),k4,_index)),
       ),
       Form(
         autovalidateMode: AutovalidateMode.always,
@@ -277,14 +306,14 @@ class _AddJopState extends State<AddJop> {
         child: SizedBox(
             width: 300,
             child: x(
-                "skillNum", "المهارات:", ".......", Icon(Icons.account_tree))),
+                "skillNum", "المهارات:", ".......", Icon(Icons.account_tree),k6,_index)),
       ),
       Form(
         autovalidateMode: AutovalidateMode.always,
         key: k7,
         child: SizedBox(
             width: 300,
-            child: x("langNum", "اللغات:", ".......", Icon(Icons.language))),
+            child: x("langNum", "اللغات:", ".......", Icon(Icons.language),k7,_index)),
       ),
       SizedBox(
         width: 300,
@@ -339,7 +368,7 @@ class _AddJopState extends State<AddJop> {
                   z("level", "المستوى", ["مبتدأ", "متمرس", "خبير"])
                 ],
               ),
-              q(d["level"]),
+              q(d["level"],k5,_index),
             ],
           ),
         ),
@@ -387,43 +416,6 @@ class _AddJopState extends State<AddJop> {
           ],
         ),
       ),
-      // SizedBox(
-      //   width: 300,
-      //   child: Row(
-      //     children: [
-      //       Icon(Icons.account_tree),
-      //       SizedBox(
-      //         width: 10,
-      //       ),
-      //       Text("عدد الشواغر:"),
-      //       SizedBox(
-      //         width: 10,
-      //       ),
-      //       z("Vacancies", "شواغر", [
-      //         1,
-      //         2,
-      //         3,
-      //         4,
-      //         5,
-      //         6,
-      //         7,
-      //         8,
-      //         9,
-      //         10,
-      //         11,
-      //         12,
-      //         13,
-      //         14,
-      //         15,
-      //         16,
-      //         17,
-      //         18,
-      //         19,
-      //         20
-      //       ]),
-      //     ],
-      //   ),
-      // ),
       SizedBox(
         width: 300,
         child: Column(
@@ -441,4 +433,103 @@ class _AddJopState extends State<AddJop> {
     ];
     return aa[_index];
   }
+
+
+
+  bool _animate = true;
+  bool _defaultInteractions = true;
+  double _arcRatio = 0.5;
+  charts.ArcLabelPosition _arcLabelPosition = charts.ArcLabelPosition.auto;
+  charts.BehaviorPosition _titlePosition = charts.BehaviorPosition.bottom;
+  charts.BehaviorPosition _legendPosition = charts.BehaviorPosition.start;
+
+  // Data to render.
+  List<_CostsData> data = [
+    _CostsData('العنوان', 0),
+    _CostsData('العمر', 0),
+    _CostsData('الساعات', 0),
+    _CostsData('الراتب', 0),
+    _CostsData('المهارات', 0),
+    _CostsData('اللغات', 0),
+    _CostsData('المستوى', 0),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    final _colorPalettes =
+    charts.MaterialPalette.getOrderedPalettes(this.data.length);
+    return Scaffold(
+        appBar: AppBar(
+              title: Text("معلومات فرصة العمل"),
+            ),
+      body: ListView(
+        padding: const EdgeInsets.all(8),
+        children: <Widget>[
+          Container(
+            height: 400,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(topRight: Radius.circular(15.8),topLeft:Radius.circular(15.8) ),
+                color: Colors.black12
+            ),
+            child: des(),
+          ),
+          Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(bottomLeft: Radius.circular(15.8),bottomRight:Radius.circular(15.8) ),
+                color: Colors.black12
+            ),
+            height: 300,
+            child: charts.PieChart(
+              [
+                charts.Series<_CostsData, String>(
+                  id: 'Sales-1',
+                  colorFn: (_, idx) => _colorPalettes[idx].shadeDefault,
+                  domainFn: (_CostsData sales, _) => sales.category,
+                  measureFn: (_CostsData sales, _) => sales.cost,
+                  data: this.data,
+                  // Set a label accessor to control the text of the arc label.
+                  labelAccessorFn: (_CostsData row, _) =>
+                  '${row.category}: ${row.cost}',
+                ),
+              ],
+              animate: this._animate,
+              defaultRenderer: new charts.ArcRendererConfig(
+                arcRatio: this._arcRatio,
+                arcRendererDecorators: [
+                  charts.ArcLabelDecorator(labelPosition: this._arcLabelPosition)
+                ],
+              ),
+              behaviors: [
+                // Add title.
+                // charts.ChartTitle(
+                //   'Dummy costs breakup',
+                //   behaviorPosition: this._titlePosition,
+                // ),
+                // Add legend. ("Datum" means the "X-axis" of each data point.)
+                charts.DatumLegend(
+                  position: this._legendPosition,
+                  desiredMaxRows: 4,
+                ),
+              ],
+            ),
+          ),
+
+
+          //..._controlWidgets(),
+        ],
+      ),
+    );
+  }
+
+  edit(int i){
+    setState(() {
+      data[i].cost=100;
+    });
+  }
+}
+class _CostsData {
+  final String category;
+  int cost;
+
+  _CostsData(this.category, this.cost);
 }
